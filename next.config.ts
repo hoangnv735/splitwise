ff0eb2f0ed36fa3e7bbb4100a28ee5f6b29fa9ae
Hoàng Ngô Việt <deserteagleXIII@gmail.com>
2025-05-27 02:57:32 +0000

    clean

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: "/splitwise",
+  reactStrictMode: true,
+  swcMinify: true,
   typescript: {
     ignoreBuildErrors: true,
   },
--- src/ai/dev.ts
+++
@@ -1,9 +0,0 @@
-
-// This file can be used to start the Genkit developer UI
-// and register flows, etc.
-
-// import { config } from 'dotenv';
-// config();
-
-// Example: import './my-flow.js';
-// Ensure that your flows are imported so that they are registered with Genkit.
